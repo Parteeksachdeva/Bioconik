f2998ac879c44be97a55e596238918c039b22813
Amber <amber.g@navigus.in>
2023-04-10 22:34:07 +0530

    init(server): Prisma Setup.

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, (err) => {
-  if (err) return console.log(err);
-  console.log("App listening on " + PORT);
-});
